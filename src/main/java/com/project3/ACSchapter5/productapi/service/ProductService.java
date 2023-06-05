e60244feb8227d0bf5cfddd9d3951f5ffe49077c
Dev01 <guilhermebrasil.acs@outlook.com>
2023-06-05 09:39:21 -0300

    Product Service

---
+++ src/main/java/com/project3/ACSchapter5/productapi/service/ProductService.java
@@ -0,0 +1,68 @@
+package com.project3.ACSchapter5.productapi.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import com.project3.ACSchapter5.productapi.dto.ProductDTO;
+import com.project3.ACSchapter5.productapi.model.Product;
+import com.project3.ACSchapter5.productapi.repository.ProductRepository;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductService {
+    private ProductRepository productRepository;
+    public List<ProductDTO>getAll(){
+        List<Product>products = productRepository.findAll();
+        return products
+                .stream()
+                .map(ProductDTO::convert)
+                .collect(Collectors.toList());
+    }
+    public List<ProductDTO>getProductByCategoryId(long categoryId){
+        List<Product> products = productRepository.getProductByCategory(categoryId);
+        return products
+                .stream()
+                .map(ProductDTO::convert)
+                .collect(Collectors.toList());
+    }
+    public ProductDTO findByProductIdentifier(String productIdentifier){
+        Product product = productRepository.findByProductIdentifier(productIdentifier);
+        if(product != null){
+            return ProductDTO.convert(product);
+        }
+        return null;
+    }
+    public ProductDTO save(ProductDTO productDTO){
+        Product product = productRepository.save(Product.convert(productDTO));
+        return ProductDTO.convert(product);
+    }
+    public void delete(long productId){
+        Optional<Product> product = productRepository.findById(productId);
+        if(product.isPresent()){
+            productRepository.delete(product.get());
+        }
+    }
+    public ProductDTO editProduct(long id, ProductDTO dto){
+        Product product =  productRepository.findById(id).orElseThrow(()->new RuntimeException("Permission not found"));
+        if(dto.getNome() != null || !dto.getNome().isEmpty()){
+            product.setNome(dto.getNome());
+        }
+        if(dto.getPreco() != null){
+            product.setPreco((dto.getPreco()));
+        }
+        return ProductDTO.convert(productRepository.save(product));
+    }
+    public Page<ProductDTO> getAllPage(Pageable page){
+        Page<Product> users = productRepository.findAll(page);
+        return users.map(ProductDTO::convert);
+    }
+
+}
